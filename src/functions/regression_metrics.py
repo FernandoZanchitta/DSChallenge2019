80e70d9e8b12d608e20e50cf591b86f83145ca81
Davi Xie <davi_xie@hotmail.com>
2023-06-19 01:31:54 -0300

    improving models

--- src/functions/regression_metrics.py
+++ src/functions/regression_metrics.py
@@ -5,6 +5,8 @@ def regression_metrics(y_pred, y_test,model_name):
     
     rmse_ = np.sqrt(metrics.mean_squared_error(y_pred,y_test))
     r2 = metrics.r2_score(y_test, y_pred)
+    mape = metrics.mean_absolute_percentage_error(y_test, y_pred)
     print(model_name," RMSE: ", rmse_)
     print(model_name,"R2: " + str(r2))
-    return rmse_,r2
+    print(model_name,"MAPE: " + str(mape))
+    return rmse_, r2, mape
